81262ed1cd717fa76fb540ef1bbe15b960fd29d2
Mathias Havn <mekjolle@live.dk>
2020-01-20 10:15:04 +0100

    Lavet til int felterne også

--- App programmering/Opgave/app/src/main/java/com/example/app/Dataconverter.java
+++ App programmering/Opgave/app/src/main/java/com/example/app/Dataconverter.java
@@ -48,5 +48,46 @@ public class Dataconverter {
         return float_1_a;
 
     }
+
+    public static void intToMsg (int i, byte [] convert, int offset)
+    {
+        byte[] intOut = ByteBuffer.allocate(4).putInt(i).array();
+        if (ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN) {
+            convert[offset] = intOut[3];
+            convert[offset + 1] = intOut[2];
+            convert[offset + 2] = intOut[1];
+            convert[offset + 3] = intOut[0];
+        }
+        else
+        {
+            convert[offset] = intOut[0];
+            convert[offset + 1] = intOut[1];
+            convert[offset + 2] = intOut[2];
+            convert[offset + 3] = intOut[3];
+        }
+
+    }
+
+    public static int msgToInt(byte [] msg, int offset)
+    {
+        byte[] int_1 = new byte[4];
+        ByteBuffer.wrap(msg, offset, 4).get( int_1); //size of int
+        byte[] int_1_s = new byte[4];
+        if (ByteOrder.nativeOrder() == ByteOrder.BIG_ENDIAN) {
+            int_1_s[0] = int_1 [3];
+            int_1_s[1] = int_1 [2];
+            int_1_s[2] = int_1 [1];
+            int_1_s[3] = int_1 [0];
+        }
+        else
+        {
+            int_1_s[0] = int_1 [0];
+            int_1_s[1] = int_1 [1];
+            int_1_s[2] = int_1 [2];
+            int_1_s[3] = int_1 [3];
+        }
+        int int_1_a = ByteBuffer.wrap(int_1_s).getInt();
+        return int_1_a;
+    }
 }
 
